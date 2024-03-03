d47a2d22145c8f95d1b7811940a76a02dad44155
Rameshwar Gupta <rameshwargupta4444@gmail.com>
2024-03-03 18:40:05 +0530

    path updated

--- notification.js
+++ notification.js
@@ -1,25 +1,20 @@
-console.log("NS Called");
 clevertap.notifications.push({
   titleText: "Would you like to receive Notification?",
   bodyText: "Will send you only trusted notifcations and updates",
   okButtonText: "OK",
   rejectButtonText: "Cancel",
-  okButtonColor: "#f28046",
-  serviceWorkerPath: "/clevertap-web-integration/service-worker.js",
+  okButtonColor: "#0697BE",
+  serviceWorkerPath: "wittycraftthreads.github.io/clevertap_sw.js",
   okCallback: function () {
-    console.log("For Soft OK Popup");
+    console.log("Soft Popup");
     Notification.requestPermission().then(function (getperm) {
       console.log("Hard Popup Permission granted", getperm);
-      // window.location.reload();
     });
-    // clevertap.event.push("CT_OK_Click");
   },
   rejectCallback: function () {
-    console.log("For Soft NO Popup");
-    // clevertap.event.push("CT_NO_Click");
+    console.log("NO Popup");
   },
   subscriptionCallback: function () {
     console.log("For Chrome Hard Popup");
-    // clevertap.event.push("Chrome_Yes_Click");
   },
 });
